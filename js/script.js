4378a36175feea362283e1828af03e8bed07f01e
Priscylla Rocha <priscyllarocha104@gmail.com>
2025-10-02 14:22:39 -0300

    Implementa tela de Game Over com botão de reset que aponta para tela inicial e corrige pontuação

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const startBtn = document.getElementById('start-btn');
 const startScreen = document.querySelector('.start-screen');
 const gameContainer = document.querySelector('.game-container');
+const gameBoard = document.querySelector('.game-board');
+const gameOverScreen = document.querySelector('.game-over-screen');
+const resetBtn = document.getElementById('reset-btn');
 
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
@@ -15,7 +18,14 @@ let gameLoop;
 
 highScoreElement.textContent = `Recorde: ${highScore}`;
 
+startBtn.addEventListener('click', () => {
+    startScreen.style.display = 'none';
+    gameContainer.style.display = 'block';
+    startGame();
+});
+
 const jump = () => {
+
     if (!isGameOver) {
         mario.classList.add('jump');
         setTimeout(() => {
@@ -26,42 +36,34 @@ const jump = () => {
 
 function startGame() {
 
+    clearInterval(gameLoop);
+
     score = 0;
     scoreElement.textContent = `Score: ${score}`;
     isGameOver = false;
     scored = false;
 
+    /* Reset do Mario */
     mario.style.bottom = '0px';
     mario.src = './Images/mario.gif';
     mario.style.width = '120px';
     mario.style.marginLeft = '0px';
+    mario.style.animation = '';
 
+    /* Reset do pipe */
     pipe.style.animation = 'pipe-animation 1.5s infinite linear';
     pipe.style.right = '-80px';
+    pipe.style.left = '';
+
+    gameOverScreen.style.display = 'none';
+    gameBoard.style.display = 'block';
 
     gameLoop = setInterval(() => {
         const pipePosition = pipe.offsetLeft;
         const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
 
         if (pipePosition <= 100 && pipePosition > 0 && marioPosition < 111.86) {
-            pipe.style.animation = 'none';
-            pipe.style.left = `${pipePosition}px`;
-
-            mario.style.animation = 'none';
-            mario.style.bottom = `${marioPosition}px`;
-
-            mario.src = './Images/game-over.png';
-            mario.style.width = '60px';
-            mario.style.marginLeft = '45px';
-
-            isGameOver = true;
-
-            if (score > highScore) {
-                highScore = score;
-                localStorage.setItem('highScore', highScore); 
-            }
-            highScoreElement.textContent = `Recorde: ${highScore}`;
-            clearInterval(gameLoop);
+            gameOver();
         }
 
         if (pipePosition <= 0 && !scored && !isGameOver) {
@@ -73,15 +75,55 @@ function startGame() {
         if (pipePosition > window.innerWidth - 100) {
             scored = false;
         }
-
     }, 10);
 }
 
-startBtn.addEventListener('click', () => {
-    startScreen.style.display = 'none';
-    gameContainer.style.display = 'block';
-    startGame();
+function gameOver() {
+
+    clearInterval(gameLoop);
+    isGameOver = true;
+
+    /* Para aparecer a tela de game over */
+    gameOverScreen.style.display = 'flex';
+
+    const pipePosition = pipe.offsetLeft;
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+    mario.src = './Images/game-over.png';
+    mario.style.width = '60px';
+    mario.style.marginLeft = '45px';
+    mario.style.bottom = `${marioPosition}px`;
+
+    /* Para atualizar o recorde */
+    if (score > highScore) {
+        highScore = score;
+        localStorage.setItem('highScore', highScore); 
+    }
+    highScoreElement.textContent = `Recorde: ${highScore}`;
+}
+
+resetBtn.addEventListener('click', () => {
+    startScreen.style.display = 'flex';
+    gameContainer.style.display = 'none';
+
+    isGameOver = false;
+    scored = false;
+    score = 0;
+    scoreElement.textContent = `Score: ${score}`;
+
+    mario.style.bottom = '0px';
+    mario.src = './Images/mario.gif';
+    mario.style.width = '120px';
+    mario.style.marginLeft = '0px';
+    mario.style.animation = '';
+
+    pipe.style.animation = 'pipe-animation 1.5s infinite linear';
+    pipe.style.right = '-80px';
+    pipe.style.left = '';
+
+    clearInterval(gameLoop);
 });
 
 document.addEventListener('keydown', jump);
-
